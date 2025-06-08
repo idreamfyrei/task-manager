85492c2822a8de40f1cac48ce91d991cbb55d75b
idreamfyrei <saumya.x3@gmail.com>
2025-06-08 22:34:25 +0530

    add: DB connection

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () =>{
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Database connected")
+
     } catch (error) {
-        
+        return console.log(error)
     }
 }
+
+export default connectDB;
