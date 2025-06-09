1fb18c2990c49012a436053b75908399f6edcc95
idreamfyrei <saumya.x3@gmail.com>
2025-06-09 22:49:47 +0530

    add: added constants and async handler

---
+++ src/utils/async-handler.js
@@ -0,0 +1,8 @@
+// Use: to catch async errors
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
